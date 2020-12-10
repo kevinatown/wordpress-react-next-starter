c8ce464a8a25208015678f4ea4e4f8f5694988f0
kevinatown <kevinatown@gmail.com>
2020-12-09 19:15:39 -0500

    updates

--- frontend/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require('postcss-easy-import')({ prefix: '_' }),
-    require('autoprefixer')({
-      /* ...options */
-    }), // so imports are auto-prefixed too
-  ],
-};
